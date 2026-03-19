6a52a2886346985307feceb12a691bcd5344a5aa
andyh444 <47712158+andyh444@users.noreply.github.com>
2026-03-19 09:47:56 +0000

    Benchmarking skeleton project

---
+++ Frinkahedron.Benchmarks/Benchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frinkahedron.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+        }
+
+        [Benchmark]
+        public void Benchmark1()
+        {
+            // TODO: Replace this with code to benchmark
+            Thread.Sleep(50);
+        }
+
+        [Benchmark]
+        public void Benchmark2()
+        {
+            // TODO: Replace this with code to benchmark
+            Thread.Sleep(25);
+        }
+    }
+}
---
+++ Frinkahedron.Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+using Frinkahedron.Benchmarks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmarks>();
+    }
+}
