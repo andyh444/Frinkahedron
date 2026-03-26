774ce22b9a8106ccbc08ae7801567c2ecc742c4c
andyh444 <47712158+andyh444@users.noreply.github.com>
2026-03-26 16:59:40 +0000

    add ConvexHull file

---
+++ Frinkahedron/Colliders/ConvexHull.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frinkahedron.Core.Colliders
+{
+    public sealed class ConvexHull
+    {
+    }
+}
